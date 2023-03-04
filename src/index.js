5190828c8b98df29d0d2fa977ec77d6d9eba7781
Klementiev Ivan <klivin76@gmail.com>
2023-03-04 21:45:53 +0200

    fatch create

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-import './css/styles.css';
+import {fetchCountries} from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './css/styles.css';
+
 
 const DEBOUNCE_DELAY = 300;
-const 
+
+const refs = {
+   searchBox: document.querySelector(`#search-box`),
+   countryList: document.querySelector(`.country-list`),
+   countryInfo: document.querySelector(`.country-info`),
+};
+
+refs.searchBox.addEventListener(`input`, debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+e.preventDefault();
+const searchQuery = e.target.value.trim();
+clearInput()
+// const searchQuery = refs.searchBox.value.trim();
+// if (searchQuery === '') {
+//     return;
+// }
+
+// fetchCountries(searchQuery).then(fatchSucctss).catch(fatchError);
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,15 @@
+const BASE_URL = `https://restcountries.com/v2`;
+const FILTER_RESPONSE = `?fields=name, capital,
+population, flags, languages`;
+
+const fetchCountries = searchQuery => {
+return fetch(`${BASE_URL}/name/${searchQuery}?${FILTER_RESPONSE}`)
+.then(response => {
+    if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+})
+}
+
+export {fetchCountries};
